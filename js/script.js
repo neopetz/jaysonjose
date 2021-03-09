34b95ddffe72cc6b85c2bef99c8c0228fe425f98
neopetz <61999023+neopetz@users.noreply.github.com>
2021-03-09 23:50:21 +0800

    add some image and layout

--- js/script.js
+++ js/script.js
@@ -6,9 +6,24 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
     });
+
+    let typed = new Typed(".typing", {
+        strings: ["Web Developer", "Software Developer", "Android Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    let typed2 = new Typed(".typing-2", {
+        strings: ["Web Developer", "Software Developer", "Android Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    
     //toggle
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 });
+
